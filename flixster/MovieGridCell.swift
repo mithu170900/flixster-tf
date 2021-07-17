bf9f8d021a4442636d9d9070fbc184832140639b
Minh Bui <minh.bui@tcu.edu>
2021-07-16 21:58:59 -0400

    Initial update

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Minh Thu Bui on 7/16/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
